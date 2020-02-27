8d02f2a3bacfd380ceb916cd47d759c04dbd76ca
Silas Jack Morsink <silas@pandopooling.com>
2020-02-26 16:14:25 -0800

    Made notes for filterCovidData.R to outline functions for processing Coronavirus data

---
+++ filterCovidData.R
@@ -0,0 +1,13 @@
+
+# filter by type (confirmed / deaths / recovered)
+## only use scrapeCovidData to scrape the one that you need
+## then use tidyCovidData to clean it up
+
+# filer by location (province or location)  (also latitude/longitude!)
+## use scrapeCovidData to scrape everything
+## then filter by location
+## then use tidyCovidData to clean it up
+
+# filter by date
+## just use importCovidData, then filter by date
+

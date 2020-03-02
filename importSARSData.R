37ebc423913ad35f41e17e945d202961bcb8fa1e
mcolon21 <mcolon21@github.com>
2020-03-01 22:07:23 -0800

    Minor meeting changes

--- importSARSData.R
+++ importSARSData.R
@@ -1,30 +1,27 @@
-#' Imports SARS disease data
+#' Scrapes SARS disease data
 #'
-#' Imports the Kaggle SARS disease data from a public Github repository.
+#' Scrapes the Kaggle SARS disease data from a public Github repository.
 #' 
 #' @return Output is a raw dataframe in its untidy format, directly from
-#'  the Github repository 
+#'  the Github repository
+#'  
 #' @importFrom readr read_csv
 #' 
 #' @examples 
-#' importSARSData()
+#' scrapeSARSData()
 #'
-#' @export
-importSARSData <- function() {
+scrapeSARSData <- function() {
   link <- "https://raw.githubusercontent.com/mcolon21/ncov2019data/master/sars_data.csv"
-  data <- readr::read_csv(link)
+  data <- suppressMessages(readr::read_csv(link))
   return(data)
 }
 
 
-#' Tidy SARS disease data
+#' Imports SARS disease data
 #'
-#' Tidies the imported SARS disease data to conform to the necessary tidy format.
+#' Tidies and imports the SARS disease data to conform to the necessary tidy format.
 #' 
-#' @param raw_sars_df a raw, untidied dataframe of the format imported from the
-#'  Gitbhub repository by the import function
-#' 
-#' @return Output is a dataframe with columns for date (Date), location (character),
+#' @return Output is a dataframe with columns for date (Date), region (character),
 #'   value_type (character; either "cases", "deaths", or "recoveries"), and value (int)
 #'   
 #' @importFrom magrittr %>%
@@ -32,18 +29,19 @@ importSARSData <- function() {
 #' @importFrom dplyr rename select arrange
 #' 
 #' @examples
-#' data = importSARSData()
-#' tidySARSData(data)
+#' importSARSData()
 #'
-tidySARSData <- function(raw_sars_df) {
-  tidy_sars_df = raw_sars_df %>%
-    dplyr::rename(location = Country, 
+#' @export
+importSARSData <- function() {
+  sars_df = scrapeSARSData() %>%
+    dplyr::rename(region = Country, 
                   cases = `Cumulative number of cases`,
                   deaths = Number_of_deaths,
                   recoveries = `Number recovered`) %>%
     tidyr::gather(cases, deaths, recoveries, key = "value_type", value = "value") %>%
-    dplyr::select(date, location, value_type, value) %>%
+    dplyr::mutate(disease = "sars") %>%
+    dplyr::select(disease, date, region, value_type, value) %>%
     dplyr::mutate(value = as.integer(value)) %>%
-    dplyr::arrange(date, location, value_type)
-  return(tidy_sars_data)
+    dplyr::arrange(date, region, value_type)
+  return(sars_df)
 }

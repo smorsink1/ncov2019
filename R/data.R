5cc9a0a150f426e7ba9430f531d31340b2c36d3f
Silas Jack Morsink <silas@pandopooling.com>
2020-03-04 13:21:07 -0800

    Added population data (country_pop_data_raw.csv), an (unfinished) function to process it (buildPopulationMap()) and documentation in data.R

--- R/data.R
+++ R/data.R
@@ -5,6 +5,9 @@
 #'   the Zika data, and the SARS data, and the latitude and longitude associated
 #'   with each country
 #'   
+#' @name country_coordinates_map
+#' @docType data
+#'   
 #' @format A data frame with 83 rows and 6 columns:
 #' \describe{
 #'   \item{country_name}{standard name of the country}
@@ -15,7 +18,28 @@
 #'   \item{longitude}{country location in longitude}
 #' }
 #' @source \url{https://developers.google.com/public-data/docs/canonical/countries_csv}
-"country_coordinates_map"
+NULL
+
+#' Population Data for Countries
+#'
+#' A dataset with rows for countries (or groups of countries), columns for years in 
+#'   which the diseases occurred (2003 for SARS, 2016 for Zika, 
+#'   2018 for Covid (most recent year of data)), values are population
+#'   
+#' @name country_pop_data_raw
+#' @docType data
+#' 
+#' @format A .csv file with 265 rows and 5 columns
+#' \describe{
+#'   \item{country_name}
+#'   \item{country_code}
+#'   \item{pop_2003}
+#'   \item{pop_2016}
+#'   \item{pop_2018}
+#' }
+#' @source \url{https://data.worldbank.org/indicator/SP.POP.TOTL}
+NULL
+
 
 #' Zika Virus Data
 #' 

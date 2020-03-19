091a7da18cc1dc8699cb190fa6b26e4dd4fb1b14
mcolon21 <mcolon21@github.com>
2020-03-19 17:52:48 -0400

    New test files for filter and augment functions

---
+++ tests/testthat/test_augments.R
@@ -0,0 +1,67 @@
+context("Testing function for augmenting disease data")
+
+test_that("dayOfDiseaseColumn works as expected", {
+  sars <- dayOfDiseaseColumn(importSARSData(from_web = F))
+  threshold_rows <- sars[which(sars$value_type == "cases" & sars$value >= 100), ]
+  first_date <- min(threshold_rows$date)
+  
+  expect_true(which(sars$day_of_disease == 1) == which(sars$date == first_date))
+  
+  covid <- dayOfDiseaseColumn(importCovidData(), threshold = 7)
+  threshold_rows <- covid[which(covid$value_type == "cases" & covid$value >= 7), ]
+  seventh_date <- (min(threshold_rows$date) + 6)
+  
+  expect_true(which(covid$day_of_disease == 7) == which(covid$date == seventh_date))
+})
+
+
+test_that("congregateDataDates works as expected", {
+  zika_confirmed <- filterDiseaseData(importZikaData(), include_suspected = FALSE)
+  zika_congregated <- congregateDataDates(zika_confirmed)
+  prov <- "Bahia"
+  country <- "Brazil"
+  
+  zika_confirmed_selected <- zika_confirmed %>%
+    dplyr::filter(province == prov & region == country)
+  
+  zika_congregated_selected <- zika_congregated %>%
+    dplyr::filter(province == prov & region == country)
+  
+  dts <- unique(zika_congregated$date)[order(unique(zika_congregated$date))]
+  
+  found_vec = c()
+  for(d in dts) {
+    congregated_value = zika_congregated_selected$value[zika_congregated_selected$date == d]
+    found = FALSE
+    if(congregated_value > 0) {
+      rolling_d = (d - 6)
+      while(!found & rolling_d <= d) {
+        if(rolling_d %in% zika_confirmed_selected$date) {
+          if(zika_confirmed_selected$value[zika_confirmed_selected$date == rolling_d] 
+             == congregated_value)
+            found = TRUE
+        }
+        rolling_d = rolling_d + 1
+      }
+      if(!found & congregated_value == 
+         zika_congregated_selected$value[zika_congregated_selected$date == (d - 7)]) {
+        found = TRUE
+      }
+    } else {
+      data_error = FALSE
+      rolling_d = (d - 6)
+      while(!data_error & rolling_d <= d) {
+        if(rolling_d %in% zika_confirmed_selected$date) {
+          if(zika_confirmed_selected$value[zika_confirmed_selected$date == rolling_d] 
+             > 0)
+            data_error = TRUE
+        }
+        rolling_d = rolling_d + 1
+      }
+      found = !data_error
+    }
+    found_vec = c(found_vec, found)
+  }
+  
+  expect_true(length(which(!found_vec)) == 0 & length(which(found_vec)) == length(dts))
+})
---
+++ tests/testthat/test_filter.R
@@ -0,0 +1,67 @@
+context("Testing function for filtering disease data")
+
+test_that("filterDiseaseData works as expected", {
+  zika <- importZikaData(from_web = F)
+  
+  zika_confirmed <- filterDiseaseData(zika)
+  expect_true(nrow(zika[which(value_type == "cumulative_confirmed_cases"), ]) == nrow(zika_confirmed))
+  
+  zika_all <- filterDiseaseData(zika, include_suspected = TRUE)
+  expect_true(nrow(zika) == nrow(zika_all))
+  
+  zika_dates <- filterDiseaseData(zika, first_date = min(zika$date), last_date = max(zika$date))
+  expect_true(nrow(zika) == nrow(zika_dates))
+  
+  zika_specific_date <- filterDiseaseData(zika, first_date = unique(zika$date)[15], last_date = unique(zika$date)[15])
+  expect_true(length(unique(zika_specific_date$date)) == 1 & 
+                unique(zika_specific_date$date)[1] == unique(zika$date)[15])
+  
+  zika_up_to_date <- filterDiseaseData(zika, last_date = unique(zika$date)[order(unique(zika$date))][28])
+  expect_true(length(unique(zika_up_to_date$date)) > 1 & 
+                min(zika_up_to_date$date) == min(zika$date) &
+                max(zika_up_to_date$date) == unique(zika$date)[order(unique(zika$date))][28])
+  
+  zika_from_on_date <- filterDiseaseData(zika, first_date = unique(zika$date)[order(unique(zika$date))][28])
+  expect_true(length(unique(zika_from_on_date$date)) > 1 & 
+                max(zika_from_on_date$date) == max(zika$date) &
+              min(zika_from_on_date$date) == unique(zika$date)[order(unique(zika$date))][28])
+  
+  
+  zika_countries <- filterDiseaseData(zika, country = c("Brazil", "Mexico"))
+  
+  expect_true(length(unique(zika_countries$region)) == 2 &
+                "Brazil" %in% zika_countries$region & 
+                "Mexico" %in% zika_countries$region)
+  
+  zika_province <- filterDiseaseData(zika, country = c("Brazil", "Mexico", "Argentina"), 
+                                     province = c("CABA"))
+  expect_true(length(unique(zika_province$province)) == 1 &
+                "CABA" %in% zika_province$province & 
+                length(unique(zika_province$region)) == 1 & 
+                "Argentina" %in% zika_province$region)
+  
+  zika_min_and_max <- filterDiseaseData(zika, min_value = 17, max_value = 39)
+  
+  expect_true(min(zika_min_and_max) >= 17 & 
+                max(zika_min_and_max) <= 39)
+  
+  expect_error(filterDiseaseData(zika, first_date = unique(zika$date)[order(unique(zika$date))][35],
+                                 last_date = unique(zika$date)[order(unique(zika$date))][32]))
+  expect_error(filterDiseaseData(zika, min_value = 5, max_value = 3))
+  expect_error(filterDiseaseData(zika, min_value = -18))
+               
+  
+  sars <- importSARSData(from_web = F)
+  sars_all <- filterDiseaseData(sars, first_date = "2003-05-01", last_date = "2002-05-31")
+  expect_true(length(unique(sars_all$value_type)) == 3 & 
+                "cases" %in% sars_all$value_type & 
+                "deaths" %in% sars_all$value_type & 
+                "recovered" %in% sars_all$value_type)
+  
+  sars_specific_value_type = filterDiseaseData(sars, type = "recovered")
+  expect_true(length(unique(sars_all$value_type)) == 1 & 
+                "recovered" %in% sars_all$value_type)
+})
+
+
+
